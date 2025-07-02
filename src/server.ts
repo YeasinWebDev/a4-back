74390b3dc33bb2741ec8ce6230c43fe62facdc1c
ar9485727@gmail.com <ar9485727@gmail.com>
2025-07-02 10:32:48 +0600

    setUp

---
+++ src/server.ts
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config();
+
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.status(200).send("pong");
+});
+
+
+async function server() {
+  try {
+    await mongoose.connect(process.env.MONGO_URL || "");
+    console.log("Connected to MongoDB");
+    app.listen(PORT, () => console.log("Server is running on port 3000"));
+  } catch (error) {
+    console.log(error, "Failed to connect to MongoDB");
+  }
+}
+
+server();
